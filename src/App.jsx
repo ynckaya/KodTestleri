78a0cb5415b153fd3128f322e28847cfc423dfca
Yalçın <ynckaya@gmail.com>
2025-03-08 12:00:31 +0300

    Login ve Success sayfaları eklendi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { Switch, Route } from 'react-router-dom';
 import './App.css'
+import Login from './components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
+      <Route exact path="/main">
+        <h1>Main Page</h1>
+      </Route>
+      <Route exact path="/error">
+      <h1>Error Page</h1>
+      </Route>
+    </Switch>
   )
 }
 
